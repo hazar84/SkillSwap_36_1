9ee148136a628e2f48f9caa3e929ce6df1b9492b
Bulat G <gazizullinbr@yandex.ru>
2025-08-31 14:19:08 +0300

    feat: добавлены глобальные стили

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './app/App.tsx'
 import { Provider } from 'react-redux';
 import store from "../src/app/providers/store.ts"
+import './app/style/index.css';
 
 createRoot(document.getElementById('root')!).render(
 	<Provider store={store}>
