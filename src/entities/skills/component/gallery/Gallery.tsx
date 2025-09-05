699d084a945809e37c3e79be33b2273cbdbc9ed0
Sergey Zaharchenko <hazar84@yandex.ru>
2025-09-05 19:59:33 +0300

    fix: реализована логига в компоненте Gallery и отрисовка в компоненте GalleryUI. Также поправил skills.json для корректного отопражения изображений (+ удалил размер 94x94 из дериктории skills) и заменил все изоброжения в дерритории users на размер 244x244 для исклюяения размытости аватара пользователя

--- src/entities/skills/component/gallery/Gallery.tsx
+++ src/entities/skills/component/gallery/Gallery.tsx
@@ -1,91 +1,59 @@
 import React, { useState } from 'react'
-import styles from './Gallery.module.css'
+import GalleryUI from './GalleryUI'
 
 interface GalleryProps {
 	images: string[]
 }
 
 const Gallery: React.FC<GalleryProps> = ({ images }) => {
-	const [currentIndex, setCurrentIndex] = useState<number>(0)
+	const [activeIndex, setActiveIndex] = useState(0)
+	const totalImages = images.length
+	const visibleSlots = totalImages > 2 ? 3 : totalImages
 
-	const previewImages = images.filter((image) => image.includes('_92х92.jpg'))
-	const largeImages = images.filter((image) => image.includes('_324х324.jpg'))
-	const totalImages = largeImages.length
-	const totalHidden = previewImages.length - 4
+	const handleChangeImage = (index: number) => {
+		setActiveIndex(index)
+	}
 
 	const handlePrev = () => {
-		setCurrentIndex((currentIndex - 1 + totalImages) % totalImages)
+		const newIndex = (activeIndex - 1 + totalImages) % totalImages
+		setActiveIndex(newIndex)
 	}
 
 	const handleNext = () => {
-		setCurrentIndex((currentIndex + 1) % totalImages)
+		const newIndex = (activeIndex + 1) % totalImages
+		setActiveIndex(newIndex)
 	}
 
-	const getPreviewArray = (current: number) => {
-		const start = (current + 1) % totalImages
-		const indices: number[] = []
-		for (let i = 0; i < 3; i++) {
-			indices.push((start + i) % totalImages)
+	const getVisibleImages = (): (string | null)[] => {
+		const visible: (string | null)[] = []
+		let count = 0
+
+		for (let i = 1; i <= totalImages; i++) {
+			const index = (activeIndex + i) % totalImages
+			if (index !== activeIndex) {
+				visible.push(images[index])
+				count++
+			}
+			if (count === visibleSlots) break
 		}
-		return indices.map((index) => ({
-			url: previewImages[index],
-			index,
-			isActive: index === current,
-		}))
+
+		return visible
 	}
 
-	const currentPreviews = getPreviewArray(currentIndex)
+	const handlePreviewClick = (index: number) => {
+		const realIndex = (activeIndex + index + 1) % totalImages
+		handleChangeImage(realIndex)
+	}
 
 	return (
-		<div className={styles.gallery}>
-			<div className={styles.largeImageWrapper}>
-				<img
-					src={largeImages[currentIndex]}
-					alt={`marketing-large-${currentIndex}`}
-					className={styles.largeImage}
-				/>
-
-				<div className={styles.arrowButtons}>
-					<button
-						className={styles.prevButton}
-						onClick={handlePrev}
-						disabled={totalImages <= 1}
-					>
-						<img src='/icons/chevron-left.svg' alt='chevron-left' />
-					</button>
-
-					<button
-						className={styles.nextButton}
-						onClick={handleNext}
-						disabled={totalImages <= 1}
-					>
-						<img src='/icons/chevron-right.svg' alt='chevron-right' />
-					</button>
-				</div>
-			</div>
-
-			<div className={styles.previewContainer}>
-				{currentPreviews.map((item, previewIndex) => (
-					<div
-						key={item.index}
-						className={`
-                            ${styles.imageWrapper} 
-                            ${item.isActive ? styles.active : ''}
-                            ${previewIndex === 2 ? styles.highlighted : ''}
-                        `}
-					>
-						<img
-							src={item.url}
-							alt={`marketing-image-${item.index}`}
-							className={styles.previewImage}
-						/>
-						{previewIndex === 2 && totalHidden > 0 && (
-							<div className={styles.hiddenCounterOverlay}>+{totalHidden}</div>
-						)}
-					</div>
-				))}
-			</div>
-		</div>
+		<GalleryUI
+			image={totalImages > 0 ? images[activeIndex] : ''}
+			visibleImages={getVisibleImages()}
+			onPrev={handlePrev}
+			onNext={handleNext}
+			onPreviewClick={handlePreviewClick}
+			totalImages={totalImages}
+		/>
 	)
 }
 
