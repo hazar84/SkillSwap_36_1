d93d0cff276fb2d5e2fd3f6bfededf0b710f2e7a
stilooz <stilooz@yandex.ru>
2025-09-10 17:01:09 +0300

    убрал мусор из апп

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,35 +1,8 @@
-// function App() {
-// 	return <div>Hello, SkillSwap_36_1</div>
-// }
-import React from 'react'
-import { useDispatch } from './providers/store'
-import { cardsActions } from '../entities/cards/model/cardsSlice'
-import {
-	fetchCategories,
-	fetchSubcategories,
-} from '../entities/skills/model/skillsSlice'
-import { getUsers } from '../api/users-api'
-import { RelatedSkills } from '../widgets/related-skills'
-
-export default function App() {
-	const dispatch = useDispatch()
-
-	React.useEffect(() => {
-		dispatch(fetchCategories())
-		dispatch(fetchSubcategories())
-		getUsers().then((res) => {
-			if (res.success) dispatch(cardsActions.setCards(res.data))
-		})
-	}, [dispatch])
-
-	return (
-		<div style={{ padding: 24 }}>
-			<RelatedSkills categoryId='cat-01' />
-		</div>
-	)
+function App() {
+	return <div>Hello, SkillSwap_36_1</div>
 }
 
-// export default App
+export default App
 
 // import { RouterProvider } from "react-router-dom"
 // import { router } from "./providers/router"
