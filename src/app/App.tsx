eb59de6e8dea6dee0d7038a7113f9b611620e182
stilooz <stilooz@yandex.ru>
2025-09-12 14:18:07 +0300

    test comm

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,24 +1,23 @@
-import { useEffect } from "react";
-import { useDispatch } from "./providers/store";
-import { fetchCards } from "../entities/cards/model/cardsThunks";
-import { fetchCategories, fetchSubcategories } from "../entities/skills/model/skillsSlice";
-import { RouterProvider } from "react-router-dom"
-import { router } from "./providers/router"
-
+import { useEffect } from 'react'
+import { useDispatch } from './providers/store'
+import { fetchCards } from '../entities/cards/model/cardsThunks'
+import {
+	fetchCategories,
+	fetchSubcategories,
+} from '../entities/skills/model/skillsSlice'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './providers/router'
 
 function App() {
+	const dispatch = useDispatch()
 
-  const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(fetchCards())
+		dispatch(fetchCategories())
+		dispatch(fetchSubcategories())
+	}, [dispatch])
 
-  useEffect(() => {
-    dispatch(fetchCards());
-    dispatch(fetchCategories());
-    dispatch(fetchSubcategories())
-  }, [dispatch]);
-
-	return (
-    <RouterProvider router={router} />
-  )
+	return <RouterProvider router={router} />
 }
 
 export default App
