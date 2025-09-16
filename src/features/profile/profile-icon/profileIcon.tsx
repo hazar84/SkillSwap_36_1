2f56adcf6327650c63a303a8252bf0fdc8530b45
Anastasia Kalinina <kalinush.malinush@gmail.com>
2025-09-16 12:49:06 +0300

    fix(profile): исправлен компонент ProfileIcon

--- src/features/profile/profile-icon/profileIcon.tsx
+++ src/features/profile/profile-icon/profileIcon.tsx
@@ -1,5 +1,5 @@
 import type { FC } from 'react'
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from '../../../app/providers/store'
 import { selectUser, userActions } from '../../auth/model/userSlice'
@@ -11,10 +11,33 @@ export const ProfileIcon: FC = () => {
 	const user = useSelector(selectUser)
 	const [isOpen, setIsOpen] = useState(false)
 
+	const ref = useRef<HTMLDivElement>(null)
+
+	useEffect(() => {
+		const handleClickOutside = (e: MouseEvent) => {
+			if (ref.current && !ref.current.contains(e.target as Node)) {
+				setIsOpen(false)
+			}
+		}
+		const handleEsc = (e: KeyboardEvent) => {
+			if (e.key === 'Escape') setIsOpen(false)
+		}
+
+		if (isOpen) {
+			document.addEventListener('mousedown', handleClickOutside)
+			document.addEventListener('keydown', handleEsc)
+		}
+
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside)
+			document.removeEventListener('keydown', handleEsc)
+		}
+	}, [isOpen])
+
 	if (!user) return null
 
 	const handleNavigate = () => {
-		navigate('/profile')
+		navigate('/profile/personal-data')
 		setIsOpen(false)
 	}
 
@@ -25,6 +48,7 @@ export const ProfileIcon: FC = () => {
 
 	return (
 		<ProfileIconUI
+			ref={ref}
 			isOpen={isOpen}
 			onToggle={() => setIsOpen(!isOpen)}
 			onLogout={handleLogout}
--- src/features/profile/profile-icon/profileIconUI.tsx
+++ src/features/profile/profile-icon/profileIconUI.tsx
@@ -1,4 +1,4 @@
-import type { FC } from 'react'
+import { forwardRef } from 'react'
 import styles from './profileIcon.module.css'
 
 type Props = {
@@ -9,15 +9,23 @@ type Props = {
 	avatar?: string
 }
 
-export const ProfileIconUI: FC<Props> = ({isOpen, onToggle, onLogout, onNavigate, avatar, }) => {
+export const ProfileIconUI= forwardRef<HTMLDivElement, Props>(
+	({ isOpen, onToggle, onLogout, onNavigate, avatar }, ref) => {
 	return (
-		<div className={styles.wrapper}>
-			<img
-				src={avatar}
-				alt='Аватар'
-				className={styles.avatar}
+		<div className={styles.wrapper} ref={ref}>
+			<button
+				type="button"
 				onClick={onToggle}
-			/>
+				aria-haspopup="menu"
+				aria-expanded={isOpen}
+				className={styles.iconbutton}
+			>
+				<img
+					src={avatar || '/icons/user-avatar.svg'}
+					alt="Аватар"
+					className={styles.avatar}
+				/>
+			</button>
 			{isOpen && (
 				<div className={styles.dropdown}>
 					<button onClick={onNavigate} className={styles.button}>
@@ -31,3 +39,4 @@ export const ProfileIconUI: FC<Props> = ({isOpen, onToggle, onLogout, onNavigate
 		</div>
 	)
 }
+)
