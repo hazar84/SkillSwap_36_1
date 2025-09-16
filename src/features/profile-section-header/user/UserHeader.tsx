5838a4fe5f8cd95b04c35133e0ba3ff65bebb7b4
Dmitry Loshakov <loshakovdmitry@ya.ru>
2025-09-16 15:57:11 +0300

    fix: Изменен компонент ProfileSectionHeader

--- src/features/profile-section-header/user/UserHeader.tsx
+++ src/features/profile-section-header/user/UserHeader.tsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import styles from './UserHeader.module.css'
-
+import { ProfileIcon } from '../../profile/profile-icon/profileIcon'
 interface UserHeaderProps {
 	name: string
-	avatar: string
 }
 
-const UserHeader: React.FC<UserHeaderProps> = ({ name, avatar }) => {
+const UserHeader: React.FC<UserHeaderProps> = ({ name }) => {
 	return (
 		<div className={styles.userHeader}>
 			<div className={styles.userName}>{name}</div>
-			<img src={avatar} alt='avatar' className={styles.avatar} />
+			<ProfileIcon/>
 		</div>
 	)
 }
