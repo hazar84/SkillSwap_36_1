f48bf8cfa96628c2702cc8ecb7554d344246e31e
Denis Kostyuhin <den.d.xps@gmail.com>
2025-09-19 02:08:33 +0700

    fix:profile-avatar

--- src/pages/favorities/favorities.tsx
+++ src/pages/favorities/favorities.tsx
@@ -8,35 +8,35 @@ import type { RootState } from '../../app/providers/store'
 import type { TUser } from '../../shared/lib/types'
 
 export const Favorities: React.FC = () => {
-    const userState = useSelector((state: RootState) => state.user)
-    const cardState = useSelector((state: RootState) => state.cards)
-    const favorites = userState.user?.favorites || []
-    const favoritesCards = cardState.items.filter(
-        (card): card is TUser => card !== undefined && favorites.includes(card.id)
-    )
+	const userState = useSelector((state: RootState) => state.user)
+	const cardState = useSelector((state: RootState) => state.cards)
+	const favorites = userState.user?.favorites || []
+	const favoritesCards = cardState.items.filter(
+		(card): card is TUser => card !== undefined && favorites.includes(card.id)
+	)
 
-    return (
-        <>
-            <Header />
-            <div className={styles.container}>
-                <div className={styles.sidebar}>
-                    <ProfileSidebar activeItem='favorites' />
-                </div>
-                <div className={styles.content}>
-                    {favoritesCards.length === 0 ? (
-                        <p>Нет избранных карточек</p>
-                    ) : (
-                        <CardList
-                            title='Избранное'
-                            data={favoritesCards}
-                            small={false}
-                            navigateButton={'/'}
-                        />
-                    )}
-                </div>
-            </div>
-            <FooterUI />
-        </>
-    )
+	return (
+		<>
+			<Header />
+			<div className={styles.container}>
+				<div className={styles.sidebar}>
+					<ProfileSidebar activeItem='favorites' />
+				</div>
+				<div className={styles.content}>
+					{favoritesCards.length === 0 ? (
+						<p>Нет избранных карточек</p>
+					) : (
+						<CardList
+							title='Избранное'
+							data={favoritesCards}
+							small={false}
+							navigateButton={'/'}
+						/>
+					)}
+				</div>
+			</div>
+			<FooterUI />
+		</>
+	)
 }
 export default Favorities
